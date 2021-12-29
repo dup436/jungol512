7e06ec68745476b997165be80d140010135e5432
Si7A-00 <Si7A-00@SI7A-00>
2021-12-29 10:50:55 +0900

    MA

---
+++ Pro1229/src/Pro12229.java
@@ -0,0 +1,9 @@
+public class Pro12229 {
+ public static void main(String[] args) {
+	int x = 49;
+	double y = 0.2683;
+	
+	System.out.printf("%d * %6f = %f\n", x, y, x * y);
+	System.setOut(null);
+ }
+}
